f192ad14161c23dabf3053b85584ba92c5cefe0e
Timothy Pearman <148655506+TimothyPearman@users.noreply.github.com>
2025-10-13 18:01:56 +0100

    Update GUI.py
    redefined as a class

--- Python/GUI.py
+++ Python/GUI.py
@@ -3,39 +3,46 @@ import serial
 import serial.tools.list_ports
 import tkinter as tk
 
-root = tk.Tk()                                                                      # create a new Tkinter window
+class GUI:
+    def __init__(self):
+        self.root = tk.Tk()                                                                     # create a new Tkinter window
+        self.data = ""
+        self.title = None
 
-def read_data():
-    with open("temp.txt", "r") as temp:
-        line = temp.readlines()
-        if line:
-            line = line[-1].strip()
-            print("\033[H\033[2B\033[G", end="") # move to top, move down 2 lines
-            print(f"Gui:   {line}")
+    def read_data(self):
+        with open("temp.txt", "r") as temp:
+            line = temp.readlines()
+            if line:
+                self.data = line[-1].strip()
+                self.title.config(text=f"Count: {self.data}")
+
+            #print("\033[H\033[2B\033[G", end="") # move to top, move down 2 lines              #debug
+            #print(f"Gui:   {self.data}")                                                       #debug
         #else:
             #print("no data found in temp.txt")
-    
-    root.after(10, read_data)
 
-def display_GUI():
-    root.title("MacroPad")     
-    x = 1920 // 2
-    y = 1080 // 2                                                                   # get relative screen resolution
-    root.geometry(f"{x}x{y}")   
-    
-    #my_var = tk.IntVar()
-    #my_var.set(0)                                                                   # set the window size
+        self.root.after(10, self.read_data)
 
-    title = tk.Label(root, text="MacroPad is running...", font=("Arial", 16))       # create a label to display status
-    title.pack(pady=20)    
-    
-    #label = tk.Label(root, textvariable=my_var)
-    #label.pack()                                                                    # add padding around the label
+    def display_GUI(self):
+        self.root.title("MacroPad")
+        x = 1920 // 2
+        y = 1080 // 2                                                                           # get relative screen resolution
+        self.root.geometry(f"{x}x{y}")
 
-    button = tk.Button(root, text="Exit", command=root.quit)                        # create an exit button
-    button.pack(pady=10)                                                            # add padding around the button
+        #my_var = tk.IntVar()
+        #my_var.set(0)                                                                          # set the window size
+
+        self.title = tk.Label(self.root, text="MacroPad is running...", font=("Arial", 16))     # create a label to display status
+        self.title.pack(pady=20)    
+    
+        #label = tk.Label(root, textvariable=my_var)
+        #label.pack()                                                                           # add padding around the label
+
+        button = tk.Button(self.root, text="Exit", command=self.root.quit)                      # create an exit button
+        button.pack(pady=10)                                                                    # add padding around the button
 
 if __name__ == "__main__":
-    display_GUI()                                                                   # display the GUI
-    read_data()                                                                     # read data from temp
-    root.mainloop()                                                                 # start the Tkinter event loop
+    gui = GUI()                                                                                 # create GUI instance
+    gui.display_GUI()                                                                           # display the GUI
+    gui.read_data()                                                                             # read data from temp
+    gui.root.mainloop()                                                                         # start the Tkinter event loop
